fe912bc8c964620f12a2be0b1c5deb5afe9a6678
Hitesh Aleriya <hitesh.aleriya@naukri.com>
2017-02-17 12:59:12 +0530

    done code refactoring in modal plugin

--- modal/js/modal.js
+++ modal/js/modal.js
@@ -1,40 +1,40 @@
 var modal = (function() {
-      
-    function open(modal) {
-        modal.style.display = "block";
-        document.body.style.overflow = "hidden";
-    }
 
-    function close(modal) {
-        modal.style.display = "none";
-        document.body.style.overflow = "";
-    }
+    var plugin = {
+        open: function(modal) {
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
+        },
+        close: function(modal) {
+            modal.style.display = "none";
+            document.body.style.overflow = "";
+        },
+        init: function(container, btn) {
+            var cont = document.querySelector(container),
+                actionBtn = document.querySelector(btn),
+                closeBtn = cont.querySelector('.close');
 
-    function init(container, btn) {
-        var cont = document.querySelector(container),
-            actionBtn = document.querySelector(btn),
-            closeBtn = cont.querySelector('.close');
+            // When the user clicks the button, open the modal 
+            actionBtn.onclick = function() {
+                plugin.open(cont);
+            }
 
-        // When the user clicks the button, open the modal 
-        actionBtn.onclick = function() { 
-            open(cont);
-        } 
+            // When the user clicks on <span> (x), close the modal
+            closeBtn.onclick = function() {
+                plugin.close(cont)
+            };
 
-        // When the user clicks on <span> (x), close the modal
-        closeBtn.onclick = function() {
-            close(cont)
-        };
-
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == cont) {
-                close(cont);
+            // When the user clicks anywhere outside of the modal, close it
+            window.onclick = function(event) {
+                if (event.target == cont) {
+                    plugin.close(cont);
+                }
             }
         }
-    }
+    };
 
     return {
-        init: init
+        init: plugin.init
     }
 
 }());
